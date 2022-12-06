75dbd962d883ab89a50dd2190e825b57ae87ea88
PoshoGH <field.joe22@gmail.com>
2022-12-06 16:28:39 +0000

    LAMP Finish

--- Bash-Script.sh
+++ Bash-Script.sh
@@ -7,4 +7,12 @@ echo "Software Updated! Stand by for LAMP installation"
 
 echo "Installing Apache2"
 sudo -e apt-get install apache2 #this will install apache 2 and the -e will make sure the script closes on error to avoid a bad install
+sudo service apache2 restart
+
+echo "Installing MYSQL"
+sudo -e apt-get
+
+echo "Installing PHP"
+sudo -e apt-get install php7.0
+
 
