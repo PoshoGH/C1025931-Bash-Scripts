a8c123eea79bcbe020002f54ad5a78416b899ac8
PoshoGH <field.joe22@gmail.com>
2022-12-06 15:47:45 +0000

    Apache2

--- Bash-Script.sh
+++ Bash-Script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo -e "Upadting your software please wait."
-sudo apt-get update -y && sudo apt-get upgrade -y
+echo "Updating your software please wait."
+sudo apt-get update -y && sudo apt-get upgrade -y #this updates the software so that we run into less issues going forward with the install
+
+echo "Software Updated! Stand by for LAMP installation"
+
+echo "Installing Apache2"
+sudo -e apt-get install apache2 #this will install apache 2 and the -e will make sure the script closes on error to avoid a bad install
+
